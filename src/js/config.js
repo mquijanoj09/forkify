1ddd03f16e573d5703f4e8f3c9ab363e4f885f9a
mquijanoj09 <mquijanoj@hotmail.com>
2023-02-02 22:54:40 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '20e7aac0-325c-4ee0-93fe-de2d56710f19';
+export const MODAL_CLOSE_SEC = 2.5;
